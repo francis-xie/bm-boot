85496cbbac12a7058b26a949cc7f31cfdcf3003b
francis-xie <1159394241@qq.com>
2020-05-22 17:59:31 +0800

    2020-05-22 调试

---
+++ bm-src3/lib/clean-iReport.bat
@@ -0,0 +1,23 @@
+rem Chitty 2012/08/08   iReport¡–”°À˘–ËJAR
+
+del barbecue-1.5-beta1.jar
+del barcode4j-2.0.jar
+del commons-beanutils-1.8.2.jar
+del commons-collections-3.2.1.jar
+del commons-dbcp-1.2.2.jar
+del commons-digester-1.7.jar
+del commons-javaflow-20060411.jar
+del commons-logging-1.1.jar
+del commons-math-1.0.jar
+del commons-pool-1.3.jar
+del commons-vfs-1.0.jar
+del groovy-all-1.5.5.jar
+del iText-2.1.0.jar
+del iTextAsian.jar
+del jasperreports-3.7.1.jar
+del jasperreports-chart-themes-3.6.2.jar
+del jasperreports-extensions-3.5.3.jar
+del jasperreports-fonts-3.6.1.jar
+del jfreechart-1.0.12.jar
+del spring.jar
+
